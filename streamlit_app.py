714ad57e105815a646729ed13474ae9fad7ea497
Goth-AppDev <108154100+Goth-AppDev@users.noreply.github.com>
2022-06-25 02:36:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.title('My Parent New Healthy Diner')
 
 streamlit.title('Break Fast Menu')
 streamlit.text('Starter : Coffee / Tea')
+streamlit.text('Tiffin : Fruite/Raggi/Millets')
+streamlit.text('MainCoure : Brown Rice - veggies')
